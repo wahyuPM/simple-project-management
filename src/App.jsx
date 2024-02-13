7914050f10fc2acfb5caf35905736fffed7599a4
wahyuPM <wahyupratama191@gmail.com>
2024-02-13 23:39:25 +0800

    add : no project component add : split button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar></ProjectSidebar>
-      <NewProject></NewProject>
+      {/* <NewProject></NewProject> */}
+      <NoProjectSelected />
     </main>
   );
 }
