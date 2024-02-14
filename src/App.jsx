7dfb2c160269f2700546c8e936518caa566339b3
wahyuPM <wahyupratama191@gmail.com>
2024-02-14 08:07:03 +0800

    add : no project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  const handleStartAddProject = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar></ProjectSidebar>
-      {/* <NewProject></NewProject> */}
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject}></ProjectSidebar>
+      {content}
     </main>
   );
 }
